88dbfd906a93a3e3f4d1cd3cfb5daaedab1cfb4c
Tal Ron <github@talron.dev>
2023-11-26 16:23:41 +0200

    contact details

--- src/pages/pet-found/index.tsx
+++ src/pages/pet-found/index.tsx
@@ -4,9 +4,12 @@ import styled from 'styled-components'
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import Axios from 'axios'
-import { Paths } from '../../app/consts'
+import { Color, Paths } from '../../app/consts'
 import { ButtonStyle } from '@/app/styles/ButtonStyle'
 import { Pet } from '@/app/types/pet'
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { IoChatboxOutline } from "react-icons/io5";
 
 type FoundPet = {
   email: string;
@@ -21,11 +24,21 @@ const PetFoundPageContainer = styled.div`
   & > h2 {
     font-size: 2rem;
     text-align: center;
+    margin-bottom: 1rem;
   }
 `
 
 const PetImageContainer = styled.div`
-  border-radius: 1rem;  
+  border-radius: 2rem;
+  height: 600px;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top center;
+  }
 `
 
 const ButtonsContainer = styled.div`
@@ -108,15 +121,56 @@ const ConfirmPageStyle = styled.div`
   }
 `
 
+const ConfirmPageMetaStyle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  font-size: 1.5rem;
+`
+
+const DetailsContainer = styled.div`
+  border: 4px solid ${Color.borders};
+  padding: 2rem;
+  border-radius: 2rem;
+  width: 100%;
+
+  & > p {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+  }
+
+  & > button {
+    margin-top: 1rem;
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
+`
+
 function ConfirmPage(
   { foundPet }:
   { foundPet: FoundPet }
 ) {
   return (
     <ConfirmPageStyle>
-      <h2>Amazing! 🎉</h2>
-      <p>Finder phone: {foundPet.phone}</p>
-      <p>Finder email: {foundPet.email}</p>
+      <h2>Congratulation! 🎉</h2>
+
+      <DetailsContainer>
+        <p>Finder details:</p>
+        <ConfirmPageMetaStyle>
+          <FaPhoneAlt />
+          <span>{foundPet.phone}</span>
+        </ConfirmPageMetaStyle>
+        <ConfirmPageMetaStyle>
+          <MdEmail />
+          <span>{foundPet.email}</span>
+        </ConfirmPageMetaStyle>
+        <ButtonStyle disabled={true}>
+          <IoChatboxOutline />
+          Live Chat
+        </ButtonStyle>
+      </DetailsContainer>
+
     </ConfirmPageStyle>
   )
 }
